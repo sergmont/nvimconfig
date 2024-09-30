1dcc82effb0aae01a212c814a41ac2cd9079243a
sergmont <sergiomontmart@gmail.com>
2024-09-30 12:12:00 -0600

    updated configurations, removed comments, added instructions

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,16 +1,19 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-  config = function()
-    local comment = require("Comment")
+	"numToStr/Comment.nvim",
+	-- https://github.com/numToStr/Comment.nvim
+	-- gcc for line, gbc for blocks and add count at the beginning for specific amount of lines. ex. 5gbc 5 lines in block. More information on the github page
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	config = function()
+		local comment = require("Comment")
 
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx jsx svelte html
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+		-- enable comment
+		comment.setup({
+			-- for commenting tsx jsx svelte html
+			pre_hook = ts_context_commentstring.create_pre_hook(),
+		})
+	end,
 }
